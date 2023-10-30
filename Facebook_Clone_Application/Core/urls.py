886cfb7c52e7ea353d03eed0310b84fb5cad5571
wajidkorkani <errormrcoder@gmail.com>
2023-10-30 16:17:51 +0500

    Now user can create his/her profile and open profile page.

--- Facebook_Clone_Application/Core/urls.py
+++ Facebook_Clone_Application/Core/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path 
 from Core.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('home/', home, name='home'),
-]
+    path('create-user-profile/', Create_User_Profile.as_view(), name='Create_User_Profile'),
+    path('me/', current_user_profile, name='current_user_profile'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Facebook_Clone_Application/Core/views.py
+++ Facebook_Clone_Application/Core/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
 # Create your views here.
 def home(request):
     template = 'Core/home.html'
     return render(request, template)
+
+# Current user section
+# Creating user profile 
+class Create_User_Profile(LoginRequiredMixin, CreateView):
+    model = UserProfile
+    template_name = 'Core/current-user/Create_User_Profile.html'
+    success_url = '/home/'
+    fields = ['image', 'city', 'country']
+    def form_valid(self, form):
+        # Automatically set the user field to the currently logged-in user
+        form.instance.user = self.request.user
+        form.instance.fname = self.request.user.first_name
+        form.instance.lname = self.request.user.last_name
+        return super().form_valid(form)
+    
+# Current user profile page
+def current_user_profile(request):
+    try:
+        profile = get_object_or_404(UserProfile, user=request.user)
+        template = 'Core/current-user/current-user-profie-page.html'
+        context = {
+            'user_profile': profile,
+        }
+        return render(request, template, context)
+    except:
+        return redirect('/create-user-profile/')
+    
