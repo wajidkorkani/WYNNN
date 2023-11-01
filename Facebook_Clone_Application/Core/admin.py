decc71129a24776f90d38d5a24d31089b90d3632
Wajid Korkani <143551139+wajidkorkani@users.noreply.github.com>
2023-11-01 11:41:58 +0500

    Update admin.py
    Now super user can control everything by using admin panel.

--- Facebook_Clone_Application/Core/admin.py
+++ Facebook_Clone_Application/Core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(UserPost)
+admin.site.register(Blog)
+admin.site.register(BlogComment)
+admin.site.register(BlogCommentReply)
