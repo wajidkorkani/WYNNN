df1abacaafeea6b502f0b0845e2b7ea27697b525
Wajid Korkani <errormrcoder@gmail.com>
2024-06-30 17:55:03 +0500

    Commits are added in the urls.py file of the Auth app and it will help other users to understand that what each url is for and what it will do.

--- Facebook_Clone_Application/Auth/urls.py
+++ Facebook_Clone_Application/Auth/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path 
 from Auth.views import *
+
 urlpatterns = [
+    # For signup 
     path('signup/', Registration, name='signup'),
+    
+    # For login/signin
     path('', login, name='login'),
+    
+    # For logout/signout
     path('logout/', user_logout_option, name='logout'),
+ 
+    # To verify the email address given by the user to create user account
     path('verify_otp/', verify_otp, name='verify_otp'),
 ]
