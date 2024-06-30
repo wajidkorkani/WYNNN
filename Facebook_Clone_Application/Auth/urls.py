3bf76bc6384d3a7b5ae121239472b2af9a01d61a
Wajid Korkani <errormrcoder@gmail.com>
2024-06-30 23:43:13 +0500

    Comments are added in the urls file of auth app so understand the code better.

--- Facebook_Clone_Application/Auth/urls.py
+++ Facebook_Clone_Application/Auth/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path 
 from Auth.views import *
+
 urlpatterns = [
+
+    # This url is for signup
     path('signup/', Registration, name='signup'),
+
+    # This url is for login 
     path('', login, name='login'),
+
+    # This url is for logout 
     path('logout/', user_logout_option, name='logout'),
+
+    # This url is for email otp verification 
     path('verify_otp/', verify_otp, name='verify_otp'),
 ]
