085e7d0d9a86b200e80443cdfe151e72ecc69375
Wajid Korkani <errormrcoder@gmail.com>
2024-06-30 17:50:21 +0500

    One commit is added about registration form.

--- Facebook_Clone_Application/Auth/forms.py
+++ Facebook_Clone_Application/Auth/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+# This is Registration Form 
 class RegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
